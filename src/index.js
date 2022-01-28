4796295b51afba3691909931d051ee863f9a5094
Ani Tarverdyan <ani.tarverdyn.87@gmail.com>
2022-01-28 23:31:52 +0400

    publish to npm

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Avatar from "./lib/avatar";
-import AvatarGroup from "./lib/avatarGroup";
-import Button from "./lib/button";
-
-export { Button, Avatar, AvatarGroup };
---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import Avatar from "./avatar";
+import AvatarGroup from "./avatarGroup";
+import Button from "./button";
+
+export { Button, Avatar, AvatarGroup };
