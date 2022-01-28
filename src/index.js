35ec44698152dab27b527df2c626072041fd7f9d
Ani Tarverdyan <ani.tarverdyan.87@gmail.com>
2022-01-28 22:41:41 +0400

    publish to npm

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import React from "react"
-import ReactDom from "react-dom"
-import Button from "./lib/button"
-export {Button}
+import Avatar from "./lib/avatar";
+import AvatarGroup from "./lib/avatarGroup";
+import Button from "./lib/button";
 
-ReactDom.render(
-    <React.StrictMode>
-        <div>
-            <Button
-                label={'Buttonchik'}
-                iconStart={'https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg'}
-                // iconEnd={'https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg'}
-                color={'warning'}
-                size={'large'}
-                disabled={false}
-                onClick={() => alert('asdf')}
-            />
-        </div>
-    </React.StrictMode>,
-    document.getElementById("root"))
+export { Button, Avatar, AvatarGroup };
