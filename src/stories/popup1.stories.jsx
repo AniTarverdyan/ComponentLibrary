aea3f7507da17d1f91a9457e915c83e1e3b5c793
Edvard-Avagyan <edvard_avagyan94@mail.ru>
2022-01-30 16:33:55 +0400

    "create component"

--- src/stories/popup1.stories.jsx
+++ src/stories/popup1.stories.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import Popup from "../../lib/popup1/popup1";
+import Popup from "../lib/popup1/popup1";
 
 export default {
   title: "Library/Popup1",
   component: Popup,
 };
 
+function foo(){
+  alert("check")
+}
+
 const Template = (args) => <Popup {...args} />;
 
 export const example1 = Template.bind({});
   example1.args = {
-    type: "login",
-    headerText: "Գրանցում",
+    type:"login",
+    headerText:"Գրանցում",
     label1Text:"անուն/ազգանուն",
-    input1Placeholder: "Գրեք Ձեր անունը/ազգանունը",
+    input1Placeholder:"Գրեք Ձեր անունը/ազգանունը",
+    input1Value:"",
+    input1onChange:"",
     label2Text:"էլեկտրոնային հասցե",
     input2Placeholder:"Գրեք Ձեր էլեկտրոնային հասցեն",
+    input2Value:"",
+    input2onChange:"",
     label3Text:"գաղտնաբառ",
-    input3Placeholder: "*******",
+    input3Placeholder:"*******",
+    input3Value:"",
+    input3onChange:"",
     buttonText:"Հաստատել",
+    buttononClick:foo,
+    message1Text:"",
+    message2Text:"",
+    message3Text:""
 };
 
 
---
+++ src/stories/popup2.stories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Popup from "../lib/popup2/popup2";
+
+export default {
+  title: "Library/Popup2",
+  component: Popup,
+};
+
+function foo(){
+  alert("check")
+}
+
+const Template = (args) => <Popup {...args} />;
+
+export const example1 = Template.bind({});
+  example1.args = {
+    type:"subscribe",
+    headerText:"Enter your email below to get 10% OFF your first order!",
+    inputPlaceholder:"Write email",
+    inputValue:"",
+    inputonChange:"",
+    buttonText:"Subscribe",
+    buttononClick: foo,
+    linkText:"No thanks. I will pay full price",
+    linkHref:"",
+    messageText:""
+};
+
+
+
+
+
