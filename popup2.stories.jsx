bd76a3579f50e48df8df1516453e66ff5955f100
Edvard-Avagyan <edvard_avagyan94@mail.ru>
2022-01-30 15:39:29 +0400

    "create popup2"

---
+++ popup2.stories.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Popup from "./src/lib/popup2/popup2";
+
+export default {
+  title: "Library/Popup2",
+  component: Popup,
+};
+
+const Template = (args) => <Popup {...args} />;
+
+export const example1 = Template.bind({});
+  example1.args = {
+    type: "subscribe",
+    headerText: "Enter your email below to get 10% OFF your first order!",
+    inputPlaceholder: "Write email",
+    buttonText:"Subscribe",
+    linkText:"No thanks. I will pay full price",
+};
+
+
+
+
+
