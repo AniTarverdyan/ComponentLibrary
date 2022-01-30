3a13072c19cbd95e4c2d289db8903817788dfa16
Edvard-Avagyan <edvard_avagyan94@mail.ru>
2022-01-30 15:31:46 +0400

    "create popup1"

---
+++ popup1.stories.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Popup from "./src/lib/popup1/popup1";
+
+export default {
+  title: "Library/Popup1",
+  component: Popup,
+};
+
+const Template = (args) => <Popup {...args} />;
+
+export const example1 = Template.bind({});
+  example1.args = {
+    type: "login",
+    headerText: "Գրանցում",
+    label1Text:"անուն/ազգանուն",
+    input1Placeholder: "Գրեք Ձեր անունը/ազգանունը",
+    label2Text:"էլեկտրոնային հասցե",
+    input2Placeholder:"Գրեք Ձեր էլեկտրոնային հասցեն",
+    label3Text:"գաղտնաբառ",
+    input3Placeholder: "*******",
+    buttonText:"Հաստատել",
+};
+
+
+
+
+
